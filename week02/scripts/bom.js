028150d4822c397b72ae7e0fd1bf1caa7bf41dd5
johnny00ferreira <johnnyferreiradasilva7@gmail.com>
2026-01-14 18:35:05 -0300

    add ajuste

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,31 @@
 const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
+const button = document.querySelector('#button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-li.textContent = input.value;
+button.addEventListener('click', function () {
 
-const deleteButton = document.createElement('button');
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'X';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = '';
+        input.focus();
+
+    } else {
+        input.focus();
+    }
+    
+
+});
